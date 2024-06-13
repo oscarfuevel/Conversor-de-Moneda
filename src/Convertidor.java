b79da29b6bb6c9525a554ae22371790b70b8f46b
oscarfuevel <oscarf79@yahoo.com>
2024-06-12 21:15:32 -0500

    Se realiza algunas modificaciones a la clase Convertidor

--- src/Convertidor.java
+++ src/Convertidor.java
@@ -16,31 +16,23 @@ public class Convertidor {
         String APIKey = "fe071af15fa93116af8a9ac9";
 
         try {
-            // Construir la URL de la API con los parámetros de conversión
             URL url = new URL("https://v6.exchangerate-api.com/v6/" + APIKey + "/pair/" + monedaInicial + "/" + monedaFinal + "/" + cantidad);
-
-            // Establecer la conexión HTTP
             HttpURLConnection request = (HttpURLConnection) url.openConnection();
             request.connect();
 
-            // Verificar el código de respuesta HTTP
             if (request.getResponseCode() == 200) {
-                // Analizar la respuesta JSON
                 JsonObject root = JsonParser.parseReader(new InputStreamReader(request.getInputStream())).getAsJsonObject();
 
-                // Verificar si los elementos necesarios no son nulos
                 if (root.has("conversion_result") && root.has("conversion_rate") && root.has("time_last_update_utc")) {
-                    // Obtener la tasa de conversión y reemplazar la coma por un punto
+
                     String tasaConversionStr = root.get("conversion_rate").getAsString();
                     tasaConversionStr = tasaConversionStr.replace(",", ".");
                     double tasaConversion = Double.parseDouble(tasaConversionStr);
                     String ultimaActualizacion = root.get("time_last_update_utc").getAsString();
                     System.out.println("La tasa de conversión es de: $ " + tasaConversion + " " + monedaInicial + " \nFecha: " + ultimaActualizacion);
 
-                    // Obtener el resultado de la conversión
                     double conversionResult = root.get("conversion_result").getAsDouble();
 
-                    // Formatear el resultado de la conversión
                     resultado = conversionResult;
                     System.out.println("El Resultado de la Conversion es: " + resultado + " " + monedaFinal);
                 } else {
@@ -49,7 +41,6 @@ public class Convertidor {
             } else {
                 System.out.println("Error de Respuesta " + request.getResponseCode());
             }
-            // Cerrar la conexión HTTP
             request.disconnect();
 
         } catch (MalformedURLException e) {
